292e918a9c224e776c53a25cfb0d89cf5686e01e
Даниил Пронин <mail@grawl.ru>
2016-09-04 12:05:54 +1000

    remove completely broken statement from shell script

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
-function build {
-    npm run gulp
-}
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
-    echo "Skipping deploy; just doing a build."
-    git checkout master
-    build
-    exit 0
-fi
 git checkout master
-build
+npm run gulp
 if [ -z `git diff --exit-code` ]; then
     echo "No changes to the output on this push; exiting."
     exit 0
